0a47043ed6221d8504a5624cabd8e1e359a63637
Tom Janssens <tom@corebvba.be>
2015-05-22 22:36:05 +0200

    Clean it up a bit

--- RicardoDeSilva/Domain/Delivery.cs
+++ RicardoDeSilva/Domain/Delivery.cs
@@ -2,10 +2,7 @@
 using RicardoDeSilvaBoundedContext.Modules;
 using RicardoDeSilvaBoundedContext.Repositories;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RicardoDeSilvaBoundedContext.Domain
 {
